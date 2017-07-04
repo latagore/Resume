1a3825be73a905cd68bf92c8e4af7eb8c7c13703
Steven Xu <s@stevenxu.xyz>
2017-07-04 19:13:22 +0000

    create Experience component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 
+const Experience = (data, key) => {
+  console.log(data.info)
+  return (
+    <div key={key}>
+      <h3 className="heading">{data.title} - {data.organization}</h3>
+      <div className="location">{data.location}</div>
+      <div className="date">{data.startDate} to {data.endDate}</div>
+      <ul className="info">
+      {
+        data.info.map(i => <li>{i}</li>)
+      }
+      </ul>
+    </div>
+  );
+}
+
 class App extends Component {
+
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <section class="experience">
+        {this.props.data.experience.map((e, i) => Experience(e, i))}
+      </section>
     );
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,10 @@
 
 // Load up the application styles
 require("../styles/application.scss");
-
+const data = require("../resume-data.json");
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+ReactDOM.render(<App data={data}/>, document.getElementById('react-root'));
