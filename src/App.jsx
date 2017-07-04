7bfc6a65c116e93f32880ecad062c669cc4aeb9e
Steven Xu <s@stevenxu.xyz>
2017-07-04 23:56:29 +0000

    add SkillGraph component

--- src/App.jsx
+++ src/App.jsx
@@ -38,15 +38,55 @@ const Education = (data, key) => {
   );
 }
 
+const SkillGraph = (skills) => {
+  // create an array of empty elements
+  const labels = new Array(10).fill(undefined);
+  labels[3 - 1] = "Basic";
+  labels[6 - 1] = "Professional";
+  labels[8 - 1] = "Expert";
+
+  return (
+    <figure>
+      <div className="skill-graph" role="presentation">
+        <div className="labels">
+        {
+          labels.map(
+            (label, i) => label && <span key={i} className="label-{i}" role="presentation">{label}</span>
+          )
+        }
+        </div>
+        <div className="bars">
+          {
+            skills.map(
+              (skill, key) => (
+                <div key={key} className="bar-{skill.strength * 10}">
+                  <div>{skill.name}</div>
+                  {skill.subskills && <div className="subskills">{skill.subskills.join(", ")}</div>}
+                </div>
+              )
+            )
+          }
+        </div>
+      </div>
+    </figure>
+  )
+}
+
 class App extends Component {
 
   render() {
     return (
       <div>
         <section className="education">
+          <h2>Education</h2>
           {this.props.data.education.map((e, i) => Education(e, i))}
         </section>
+        <section className="skills">
+          <h2>Skills</h2>
+          {SkillGraph(this.props.data.skills)}
+        </section>
         <section className="experience">
+          <h2>Experience</h2>
           {this.props.data.experience.map((e, i) => Experience(e, i))}
         </section>
       </div>
