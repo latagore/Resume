217d5ca36dd4ba12ada7dc64da95f4154f0de4f5
Steven Xu <stevenxu@live.ca>
2017-07-04 15:36:54 -0400

    Add education component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 import React, {Component} from 'react';
 
 const Experience = (data, key) => {
-  console.log(data.info)
   return (
     <div key={key}>
       <h3 className="heading">{data.title} - {data.organization}</h3>
       <div className="location">{data.location}</div>
       <div className="date">{data.startDate} to {data.endDate}</div>
-      <ul className="info">
-      {
-        data.info.map(i => <li>{i}</li>)
+      {data.info &&
+        <ul className="info">
+        {
+          data.info.map((info, key) => <li key={key}>{info}</li>)
+        }
+        </ul>
       }
-      </ul>
+    </div>
+  );
+}
+
+const Education = (data, key) => {
+  console.log(data.gpa);
+  return (
+    <div key={key}>
+      <h3 className="heading">{data.title} - {data.organization}</h3>
+      <div className="location">{data.location}</div>
+      <div className="date">{data.startDate} to {data.endDate}</div>
+      {(data.info || data.gpa) &&
+        <ul className="info">
+          {
+            data.gpa && <li>Overall GPA: {data.gpa.overall} out of {data.gpa.max}. Major GPA: {data.gpa.major} out of {data.gpa.max}</li>
+          }
+          {
+            data.info && data.info.map((info, key) => <li key={key}>{info}</li>)
+          }
+        </ul>
+      }
     </div>
   );
 }
@@ -20,9 +42,14 @@ class App extends Component {
 
   render() {
     return (
-      <section class="experience">
-        {this.props.data.experience.map((e, i) => Experience(e, i))}
-      </section>
+      <div>
+        <section className="education">
+          {this.props.data.education.map((e, i) => Education(e, i))}
+        </section>
+        <section className="experience">
+          {this.props.data.experience.map((e, i) => Experience(e, i))}
+        </section>
+      </div>
     );
   }
 }
