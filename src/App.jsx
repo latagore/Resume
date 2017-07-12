6bd008079d6f039cf0358143b549947abefbd8d0
Steven Xu <s@stevenxu.xyz>
2017-07-12 02:34:18 +0000

    add basic highlight skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Aside from './Components/Aside.jsx';
 
 const Header = ({data}) => {
   return (
@@ -9,41 +10,30 @@ const Header = ({data}) => {
   )
 }
 
-const Aside = ({data}) => {
-  const c = data.contact;
-  return (
-    <aside className="aside">
-      <div className="contact">
-        <div className="email"><a href={`mailto:${c.email}`}>{c.email}</a></div>
-        <div className="phone"><a href={`tel:${c.phone}`}>{c.phone}</a></div>
-        <div className="address"><a href="https://goo.gl/maps/7hTSM4Ln3Wx">{c.address}</a></div>
-        <div className="github"><a href={`https://github.com/${c.github}`}>{c.github}</a></div>
-        <div className="website"><a href={`https://${c.website}`}>{c.website}</a></div>
-        <div className="linkedin"><a href={`https://www.linkedin.com/in/${c.linkedin}`}>{c.linkedin}</a></div>
-      </div>
-      <p className="summary" dangerouslySetInnerHTML={{__html: data.summary}} />
-    </aside>
-  )
-}
-
-const Info = ({data}) => {
+const Info = ({data, highlightedSkills}) => {
   if (data.description) {
-    console.log(data);
     return (
       <div className="info-bullet">
         <p>{data.description}</p>
         <ul className="skill-list">
-          {data.skills && data.skills.map((skill, i) => <li key={i}><span className="text">{skill}</span></li>)}
+          {data.skills && data.skills.map(
+            (skill, i) => {
+              let className = "text";
+              if (highlightedSkills.has(skill)) {
+                className += " highlighted";
+              }
+              return <li key={i}><span className={className}>{skill}</span></li>
+            }
+          )}
         </ul>
       </div>
     )
   } else {
-    console.log(data);
     return <div>{data}</div>
   }
 }
 
-const Experience = ({data}) => {
+const Experience = ({data, highlightedSkills}) => {
   return (
     <div className="info-block">
       <h3 className="heading">{data.title} &mdash; {data.organization}</h3>
@@ -52,7 +42,7 @@ const Experience = ({data}) => {
       {data.info &&
         <ul className="info">
         {
-          data.info.map((info, key) => <li key={key}><Info data={info} /></li>)
+          data.info.map((info, key) => <li key={key}><Info data={info} highlightedSkills={highlightedSkills} /></li>)
         }
         </ul>
       }
@@ -60,7 +50,7 @@ const Experience = ({data}) => {
   );
 }
 
-const Education = ({data}) => {
+const Education = ({data, highlightedSkills}) => {
   return (
     <div className="info-block">
       <h3 className="heading">{data.title} &mdash; {data.organization}</h3>
@@ -72,7 +62,7 @@ const Education = ({data}) => {
             data.gpa && <li>Overall GPA: {data.gpa.overall} out of {data.gpa.max}. Major GPA: {data.gpa.major} out of {data.gpa.max}</li>
           }
           {
-            data.info && data.info.map((info, key) => <li key={key}><Info data={info} /></li>)
+            data.info && data.info.map((info, key) => <li key={key}><Info data={info} highlightedSkills={highlightedSkills} /></li>)
           }
         </ul>
       }
@@ -80,11 +70,11 @@ const Education = ({data}) => {
   );
 }
 
-const Achievement = ({data}) => {
+const Achievement = ({data, highlightedSkills}) => {
   return (
     <li>
       <h3>{data.title}</h3>
-      <Info data={data} />
+      <Info data={data} highlightedSkills={highlightedSkills} />
     </li>
   )
 }
@@ -134,16 +124,33 @@ const SkillGraph = ({skills}) => {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      highlightedSkills: new Set()
+    };
+  }
+
+  _onSkillHighlightToggle = (skill) => {
+    this.setState((state) => {
+      if (state.highlightedSkills.has(skill)) {
+        state.highlightedSkills.delete(skill);
+      } else {
+        state.highlightedSkills.add(skill)
+      }
+      return state;
+    })
+  }
 
   render() {
     return (
       <div className="resume">
         <Header data={this.props.data} />
-        <Aside data={this.props.data} />
+        <Aside data={this.props.data} onSkillHighlightToggle={this._onSkillHighlightToggle}/>
         <main>
           <section className="experience">
             <h2>Experience</h2>
-            {this.props.data.experience.map((data, key) => <Experience data={data} key={key} />)}
+            {this.props.data.experience.map((data, key) => <Experience data={data} key={key} highlightedSkills={this.state.highlightedSkills} />)}
           </section>
           <section className="skills">
             <h2>Skills</h2>
@@ -151,12 +158,12 @@ class App extends Component {
           </section>
           <section className="education">
             <h2>Education</h2>
-            {this.props.data.education.map((data, key) => <Education data={data} key={key} />)}
+            {this.props.data.education.map((data, key) => <Education data={data} key={key} highlightedSkills={this.state.highlightedSkills} />)}
           </section>
           <section className="achievements">
             <h2>Projects</h2>
             <ul>
-              {this.props.data.achievements.map((data, key) => <Achievement data={data} key={key} />)}
+              {this.props.data.achievements.map((data, key) => <Achievement data={data} key={key} highlightedSkills={this.state.highlightedSkills}/>)}
             </ul>
           </section>
         </main>
